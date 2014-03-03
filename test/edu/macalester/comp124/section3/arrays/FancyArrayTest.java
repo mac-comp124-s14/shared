fe9236007d2f83e93f24acfa2f0be18e5ca8d51c
shilad <shilad@gmail.com>
2014-03-03 15:22:37 -0600

    Array examples

--- test/edu/macalester/comp124/section3/arrays/FancyArrayTest.java
+++ test/edu/macalester/comp124/section3/arrays/FancyArrayTest.java
@@ -16,4 +16,40 @@ public class FancyArrayTest {
         FancyArray array = new FancyArray();
         assertEquals(0, array.getLength());
     }
+
+    @Test
+    public void testAddOnce() {
+        FancyArray array = new FancyArray();
+        array.add("Dog");
+        assertEquals(1, array.getLength());
+        assertEquals("Dog", array.get(0));
+    }
+
+    @Test
+    public void testAddTwice() {
+        FancyArray array = new FancyArray();
+        array.add("Dog");
+        array.add("Cat");
+        assertEquals(2, array.getLength());
+        assertEquals("Cat", array.get(1));
+        assertEquals("Dog", array.get(0));
+    }
+
+    @Test
+    public void testSet() {
+        FancyArray array = new FancyArray();
+        array.add("Dog");
+        array.add("Cat");
+        assertEquals(2, array.getLength());
+        assertEquals("Cat", array.get(1));
+        assertEquals("Dog", array.get(0));
+
+        array.set(0, "Mouse");
+        assertEquals(2, array.getLength());
+        assertEquals("Mouse", array.get(0));
+
+        array.set(1, "Rat");
+        assertEquals(2, array.getLength());
+        assertEquals("Rat", array.get(1));
+    }
 }
